5d86ed9db96c6abe19eb23001e2c55e3b99caa6d
Mohd Arman <armansheakh987@gmail.com>
2024-12-26 17:31:50 +0530

    working on authorization and JWT token

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,29 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+// old version
+// const handleDuplicateFieldsDB = (err) => {
+//   const value = err.message.match(/(["'])(\\?.)*?\1/);
+//   console.log('Value boom:', value);
+//   const message = `Duplicate field value: x. Please use another value!`;
+// };
+
+// New
+const handleDuplicateFieldsDB = (err) => {
+  const field = Object.keys(err.keyValue)[0];
+  const value = err.keyValue[field];
+  console.log(value);
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -20,6 +43,7 @@ const sendErrorProd = (err, res) => {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
+      // stack: err.stack,
     });
 
     // Programming or other unknown error: don't leak error details
@@ -31,6 +55,8 @@ const sendErrorProd = (err, res) => {
     res.status(500).json({
       status: 'error',
       message: 'Something went very wrong!',
+      stack: err.stack,
+      originalError: err,
     });
   }
 };
@@ -44,9 +70,12 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
+    // console.log('Error 1100: ', err);
     let error = { ...err };
 
     if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(err);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(err);
 
     sendErrorProd(error, res);
   }
